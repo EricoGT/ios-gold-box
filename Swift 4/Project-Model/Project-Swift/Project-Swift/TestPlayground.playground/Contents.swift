e211fadd6c4b6ee87222f0fd514ad9d3145d7659
Erico GT <erico.gimenes@gmail.com>
2018-01-03 09:16:16 -0200

    Fix: Update playground file.

--- Swift 4/Project-Model/Project-Swift/Project-Swift/TestPlayground.playground/Contents.swift
+++ Swift 4/Project-Model/Project-Swift/Project-Swift/TestPlayground.playground/Contents.swift
@@ -2,39 +2,58 @@
 
 import UIKit
 
-//TESTES DE REFERÊNCIA: **********************************
-//var user1:User = User.init(n: "João", i: 10)
-//let user2:User = User.init(n: "Maria", i: 20)
-//var user3:User = User.init(n: "Pedro", i: 30)
-//
-//var list:Array = [user1, user2, user3]
-//
-//var list2:Array = list
-//
-//list[1] = User.init(n: "Ana", i: 40)
-//list
-//
-//var user4:User = list[1]
-//user4.idade = 15
-//list
-//
-//print("User4 - name: \(user4.name), idade: \(user4.idade)")
-//
-//list[2] = user4
-//list
-//
-//user4.idade = 99
-//list
-//list2
-//
-//
-//for i in 0...(list.count-1){
-//    var user:User = list[i]
-//    user.idade = 100
-//}
-//
-//list.append(User.init(n: "Tavarez", i: 666))
-//var user5:User = list.first!
-//user5.idade = 99
-//list
+class User{
+    var name:String
+    var idade:Int
+    
+    init(n:String, i:Int){
+        name = n
+        idade = i
+    }
+    
+    func copy()->User{
+        let copyUser:User = User.init(n:self.name, i:self.idade)
+        return copyUser
+    }
+}
 
+//TESTES DE REFERÊNCIA: **********************************
+
+/*
+var user1:User = User.init(n: "João", i: 10)
+let user2:User = User.init(n: "Maria", i: 20)
+var user3:User = User.init(n: "Pedro", i: 30)
+
+var list:Array = [user1, user2, user3]
+var list2:Array = list
+var list3:Array = list.map{$0.copy()}
+
+list[1] = User.init(n: "Ana", i: 40)
+list
+list2
+
+var user4:User = list[2]
+user4.idade = 15
+list
+list2
+
+user4.name = "Juca"
+list
+list2
+
+list3
+
+for i in 0...(list.count-1){
+    let user:User = list[i]
+    user.idade = 100
+}
+
+list.append(User.init(n: "Tavarez", i: 666))
+var user5:User = list.first!
+user5.name = "Erico"
+user5.idade = 99
+
+list
+list2
+list3
+*/
