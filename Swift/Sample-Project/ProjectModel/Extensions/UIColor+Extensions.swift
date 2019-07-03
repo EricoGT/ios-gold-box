89f604db518b8aebf81a7f4b62542899e9bd1700
Érico Gimenes Teixeira <erico.teixeira@gok.digital>
2019-07-03 13:22:07 -0300

    Chore: Na extensão UIColor foram adicionados métodos de Blend (conforme Photoshop).

--- Swift/Sample-Project/ProjectModel/Extensions/UIColor+Extensions.swift
+++ Swift/Sample-Project/ProjectModel/Extensions/UIColor+Extensions.swift
@@ -153,6 +153,7 @@ extension UIColor {
         
     }
     
+    /** Clareia a cor, modificanco o valor dos 3 canais RGB para mais 0.2. Não modifica o canal alpha. */
     func lighten(by percentage: CGFloat = 0.2) -> UIColor {
         let cc = self.components()
         return UIColor.RGBA(min(cc.red + percentage, 1.0),
@@ -161,12 +162,214 @@ extension UIColor {
                             cc.alpha)
     }
     
+    /** Escurece a cor, modificanco o valor dos 3 canais RGB para menos 0.2. Não modifica o canal alpha. */
     func darken(by percentage: CGFloat = 0.2) -> UIColor {
         let cc = self.components()
         return UIColor.RGBA(max(cc.red - percentage, 0.0),
                             max(cc.green - percentage, 0.0),
                             max(cc.blue - percentage, 0.0),
                             cc.alpha)
+    }
+    
+    //MARK: Photoshop Blend Options >> (https://github.com/Orange-W/PhotoshopBending)
+    
+    // Alpha Blending
+    func blendAlpha(coverColor: UIColor) -> UIColor {
+        let c1 = coverColor.rgbaTuple()
+        let c2 = self.rgbaTuple()
+        
+        let c1r = CGFloat(c1.r)
+        let c1g = CGFloat(c1.g)
+        let c1b = CGFloat(c1.b)
+        
+        let c2r = CGFloat(c2.r)
+        let c2g = CGFloat(c2.g)
+        let c2b = CGFloat(c2.b)
+        
+        let r = c1r * c1.a + c2r  * (1 - c1.a)
+        let g = c1g * c1.a + c2g  * (1 - c1.a)
+        let b = c1b * c1.a + c2b  * (1 - c1.a)
+        
+        return UIColor.init(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: 1.0)
+    }
+    
+    /// Darken >> B<=A: C=B; B>=A: C=A
+    func blendDarken(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { return ($0 <= $1) ? $0 : $1 }
+    }
+    
+    /// Multiply >> C = A*B
+    func blendMultiply(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { return $0 * $1 }
+    }
+    
+    /// Color Burn >> C=1-(1-B)/A
+    func blendColorBurn(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { return 1 - (1 - $0) / $1 }
+    }
+    
+    /// Linear Burn >> C=A+B-1
+    func blendLinearBurn(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { return ($1 + $0) - 1.0 }
+    }
+    
+    /// Lighten >> B>=A: C=B; B<=A: C=A
+    func blendLighten(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { return ($0 >= $1) ? $0 : $1 }
+    }
+    
+    /// Screen >> C=1-(1-A)*(1-B), 也可以写成 1-C=(1-A)*(1-B)
+    func blendScreen(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { return 1 - (1 - $1) * (1 - $0) }
+    }
+    
+    /// Color Dodge >> C=B/(1-A)
+    func blendColorDodge(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) {
+            if $1 >= 1.0 { return $1 }
+            else { return min(1.0, $0 / (1 - $1)) }
+        }
+    }
+    
+    /// Linear Dodge >> C=A+B
+    func blendLinearDodge(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { return min(1, $1 + $0) }
+    }
+    
+    /// Overlay >> B<=0.5: C=2*A*B; B>0.5: C=1-2*(1-A)*(1-B)
+    func blendOverlay(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) {
+            if $0 <= 0.5 { return 2 * $1 * $0 }
+            else { return 1 - 2 * (1 - $1) * (1 - $0) }
+        }
+    }
+    
+    /// Soft Light >> A<=0.5: C=(2*A-1)*(B-B*B)+B; A>0.5: C=(2*A-1)*(sqrt(B)-B)+B
+    func blendSoftLight(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) {
+            if $1 <= 0.5 { return (2 * $1 - 1) * ($0 - $0 * $0) + $0 }
+            else { return (2 * $1 - 1)*( sqrt($0) - $0) + $0 }
+        }
+    }
+    
+    /// Hard Light >> A<=0.5: C=2*A*B; A>0.5: C=1-2*(1-A)*(1-B)
+    func blendHardLight(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) {
+            if $1 <= 0.5 { return 2 * $1 * $0 }
+            else { return 1 - 2 * (1 - $1) * (1 - $0) }
+        }
+    }
+    
+    /// Vivid Light >> A<=0.5: C=1-(1-B)/(2*A); A>0.5: C=B/(2*(1-A))
+    func blendVividLight(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) {
+            if $1 <= 0.5 { return self.fitIn((1 - (1 - $0) / (2 * $1)), ceil: 1.0) }
+            else { return self.fitIn($0 / (2 * (1 - $1)), ceil: 1.0) }
+        }
+    }
+    
+    /// Linear Light >> C=B+2*A-1
+    func blendLinearLight(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { return self.fitIn($0 + 2 * $1 - 1, ceil: 1.0) }
+    }
+    
+    /// Pin Light
+    /// B<2*A-1:     C=2*A-1
+    /// 2*A-1<B<2*A: C=B
+    /// B>2*A:       C=2*A
+    func blendPinLight(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) {
+            if $0 <= 2 * $1 - 1 { return 2 * $1 - 1 }
+            else if (2 * $1 - 1 < $0) && ($0 < 2 * $1) { return $0}
+            else { return 2 * $1 }
+        }
+    }
+    
+    /// Hard Mix >> A<1-B: C=0; A>1-B: C=1
+    func blendHardMix(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) {
+            if $1 <= 1 - $0 { return 0 }
+            else { return 1 }
+        }
+    }
+    
+    /// Difference >> C=|A-B|
+    func blendDifference(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { abs($1 - $0) }
+    }
+    
+    /// Exclusion >> C = A+B-2*A*B
+    func blendExclusion(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { $1 + $0 - 2 * $1 * $0  }
+    }
+    
+    /// Subtract >> C=A-B
+    func blendMinus(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) { $1 - $0 }
+    }
+    
+    /// Divide >> C=A/B
+    func blendDivision(coverColor: UIColor,alpha: CGFloat = 1.0) -> UIColor {
+        return blendProcedure(coverColor: coverColor, alpha: alpha) {
+            if $0 == 0{
+                return 1.0
+            }else {
+                return self.fitIn($1 / $0, ceil: 1.0)
+            }
+        }
+    }
+    
+    //MARK: PRIVATE
+    
+    private func fitIn(_ value: CGFloat, ceil: CGFloat = 255) -> CGFloat { return max(min(value,ceil),0) }
+    
+    private func fitIn(_ value: Double, ceil: CGFloat = 255) -> CGFloat { return fitIn(CGFloat(value), ceil: ceil) }
+    
+    private func rgbaTuple() -> (r: CGFloat, g: CGFloat, b: CGFloat,a: CGFloat) {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        self.getRed(&r, green: &g, blue: &b, alpha: &a)
+        r = r * 255
+        g = g * 255
+        b = b * 255
+        
+        return ((r),(g),(b),a)
+    }
+    
+    /// Procedure
+    private func blendProcedure(
+        coverColor: UIColor,
+        alpha: CGFloat,
+        procedureBlock: ((_ baseValue: CGFloat,_ topValue: CGFloat) -> CGFloat)?
+        ) -> UIColor {
+        let baseCompoment = self.rgbaTuple()
+        let topCompoment = coverColor.rgbaTuple()
+        //
+        let mixAlpha = alpha * topCompoment.a + (1.0 - alpha) * baseCompoment.a
+        
+        // RGB
+        let mixR = procedureBlock?(
+            baseCompoment.r / 255.0,
+            topCompoment.r / 255.0)
+            ?? (baseCompoment.r) / 255.0
+        
+        let mixG = procedureBlock?(
+            baseCompoment.g / 255.0,
+            topCompoment.g / 255.0)
+            ?? (baseCompoment.g) / 255.0
+        
+        let mixB = procedureBlock?(
+            baseCompoment.b / 255.0,
+            topCompoment.b / 255.0)
+            ?? baseCompoment.b / 255.0
+        
+        
+        return UIColor.init(red:   fitIn(mixR),
+                            green: fitIn(mixG),
+                            blue:  fitIn(mixB),
+                            alpha: mixAlpha)
     }
 
 }
